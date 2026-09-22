731aea14b54b3142370fc2084e7362c204c24b51
nsukpram <67010476@kmitl.ac.th>
2026-09-22 16:10:20 +0700

    cell05

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,6 @@
+original_array = [2,8,9,48,8,22,-12,2]
+new_array = []
+for i in original_array :
+    new_array.append(i+2)
+print("Original array: ",original_array)
+print("New array: ",new_array)
---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,7 @@
+original_array = [2,8,9,48,8,22,-12,2]
+new_array = []
+for i in original_array :
+    if i > 5 :
+        new_array.append(i+2)
+print(original_array)
+print(new_array)
