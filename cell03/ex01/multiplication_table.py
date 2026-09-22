7396e820ed0e592374fecdf963e0683ef1f7ab23
nsukpram <67010476@kmitl.ac.th>
2026-09-22 11:43:43 +0700

    cell03

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,4 @@
+print("Enter a number")
+num = int(input().strip())
+for i in range(10) :
+    print(f"{i} x {num} = {i*num}")
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,3 @@
+say = input("What you gotta say? : ").strip()
+while say != "STOP" :
+    say = input("I got that! Anything else? : ").strip()
